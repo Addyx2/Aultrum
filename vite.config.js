6c8221cb769423b8dc730013bbff4e81ab8638b6
Addyx2 <addyx2@users.noreply.github.com>
2026-09-09 14:11:21 +0100

    Rebrand pilot: sky blue CTAs, goose flock hero, gateway CX rebrand fixes; remove broken inject plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import htmlInject from 'vite-plugin-html-inject';
 
 export default defineConfig({
-  plugins: [htmlInject()],
   build: {
     rollupOptions: {
       input: {
